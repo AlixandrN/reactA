b881f077d11ab63c609e766e24015891c6242d92
Alexandr <nasanovicha@gmail.com>
2022-09-12 12:24:15 +0300

    feat: add movie search and modal window

--- nasanovich-app/my-app/src/components/movielist/MovieCard.jsx
+++ nasanovich-app/my-app/src/components/movielist/MovieCard.jsx
@@ -3,11 +3,11 @@ import s from './MovieCard.module.css'
 
 const MovieCard = (props) => {
   return (
-    <div className= {s.item} >
-        <div>Movie № {props.number}</div>
-        <div> {props.item.Title}</div>
-        <img src= {props.item.Poster} />
-        <button onClick={props.f} data-id = {props.number-1} > Подробнее</button>
+    <div className={s.item} >
+      <div>Movie № {props.number}</div>
+      <div> {props.item.Title}</div>
+      <img src={props.item.Poster} />
+      <button onClick={props.f} data-id={props.number - 1} > Подробнее</button>
     </div>
   )
 }
